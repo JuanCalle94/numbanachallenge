35dec3dd5c78aec7c4db567c13bc1f33dff85d8e
juancalle94 <judacar2@gmail.com>
2022-09-19 19:03:04 -0500

    Agregamos React router y organizamos las rutas

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,25 +1,21 @@
-import logo from '../assets/img/logo.svg';
-import '../styles/App.css';
-import '../styles/app.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "../containers/Layout";
+import Productos from "../pages/Productos";
+import Carro from "../pages/Carro";
+import NotFound from "../pages/NotFound";
+import "../styles/app.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Productos />} />
+          <Route path="/carro" element={<Carro />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
