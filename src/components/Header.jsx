bc72f129fbd3a1466956d5dd58cb4dcefe900d4c
juancalle94 <judacar2@gmail.com>
2022-09-19 20:07:23 -0500

    Agregamos fuentes y iconos, Header con estilos

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from "../assets/img/numbanalogo.png";
+
+function Header() {
+  return (
+    <>
+      <header className="container header">
+        <img
+          className="header__logo"
+          src={logo}
+          alt="logo"
+          width="60"
+        />
+        <a className="header__carrito" href="/">
+            Carrito
+            <i class="fa-solid fa-cart-shopping"></i>
+        </a>
+      </header>
+    </>
+  );
+}
+
+export default Header;
